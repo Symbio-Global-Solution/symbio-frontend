27b9ef017842c57ffc90014c16f8a0fa9701a7c9
HenriqueTcesar <rickcesar1717@gmail.com>
2025-11-22 16:41:04 -0300

    feat: Configuração do tailwindcss index.css.

--- symbio-front/tailwind.config.js
+++ symbio-front/tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class', 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        symbio: {
+          dark: '#000010',      
+          light: '#F0F0F5',      
+          purple: '#764A95',     // Primária
+          neon: '#B265D9',       // Brilho
+          glass: 'rgba(255, 255, 255, 0.05)',
+        }
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        display: ['Clash Display', 'sans-serif'], // Fonte do Título
+      },
+    },
   },
   plugins: [],
 }
