fbf51ef1038afe4bb9548507f90164d1a985b245
HenriqueTcesar <rickcesar1717@gmail.com>
2025-11-23 11:43:28 -0300

    feat: Adicionando a página de faq e as configurações do faqdata.

--- symbio-front/src/App.tsx
+++ symbio-front/src/App.tsx
@@ -1,11 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from './context/ThemeContext';
+import { ThemeProvider } from './Context/ThemeContext';
 import Menu from './components/Menu/Menu';
 
-import Home from './routes/Home';
-import Equipe from './routes/Equipe';
-import Sobre from './routes/Sobre';
-import Faq from './routes/Faq';
+import Home from './pages/Home';
+import Faq from './pages/Faq';
 
 function App() {
   return (
@@ -17,8 +15,6 @@ function App() {
           <main className="flex-grow pt-28 px-4">
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/equipe" element={<Equipe />} />
-              <Route path="/sobre" element={<Sobre />} />
               <Route path="/faq" element={<Faq />} />
             </Routes>
           </main>
